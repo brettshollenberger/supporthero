62275e8c36f36cbc8b9144cf662abe3d0e46cbc6
Brett Cassette <brett.shollenberger@gmail.com>
2015-02-16 23:11:17 -0800

    Add initial seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,25 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+unless User.count > 0
+  ["Sherry Pace", "Boris Gardner", "Vicente Taggart", "Matte Aptekar", "Jack Cassel",
+   "Kevin Jenkins", "Zoe Dholakia", "Jay Wilds", "Eadon Eastman", "Franky Weber", "Luis Helene",
+   "James Rainone"].each do |name|
+     first_name, last_name = name.split(" ")[0], name.split(" ")[1]
+     email = "#{first_name}.#{last_name}@gmail.com"
+
+     User.create(first_name: first_name, last_name: last_name, email: email, password: "foobar15")
+   end
+end
+
+Assignment.delete_all
+
+%w(Sherry Boris Vicente Matte Jack Sherry Matte Kevin Kevin Vicente Zoe Kevin Matte Zoe Jay Boris Eadon Sherry Franky Sherry Matte Franky Franky Kevin Boris Franky Vicente Luis Eadon Boris Kevin Matte Jay James Kevin Sherry Sherry Jack Sherry Jack).each.with_index do |first_name, index|
+
+  user          = User.where(first_name: first_name).first
+  date          = Date.today + index.days
+  month         = date.month
+  day           = date.day
+  year          = date.year
+  calendar_date = CalendarDate.where(month: month, day: day, year: year).first
+
+  Assignment.create(calendar_date: calendar_date, user: user)
+
+end
