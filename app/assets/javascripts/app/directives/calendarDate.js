42302ceb589777a17c051ef96ae54738e4e44326
Brett Cassette <brett.shollenberger@gmail.com>
2015-02-16 21:38:25 -0800

    Finish major styling

--- app/assets/javascripts/app/directives/calendarDate.js
+++ app/assets/javascripts/app/directives/calendarDate.js
@@ -3,6 +3,7 @@ angular
   .directive('calendarDate', [function() {
     return {
       templateUrl: "views/directives/calendar_date.html",
+      replace: true,
       link: function(scope, element, attrs) {
       }
     }
