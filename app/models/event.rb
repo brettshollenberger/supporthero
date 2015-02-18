b749bf4565582f59e576d38a640b1a38709dbea9
Brett Cassette <brett.shollenberger@gmail.com>
2015-02-18 02:57:15 -0800

    Add dependent => destroy to event recipients

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,7 +2,7 @@ class Event < ActiveRecord::Base
   belongs_to :event_type
   belongs_to :eventable, polymorphic: true
   belongs_to :creator, :class_name => "User", :foreign_key => "creator_id"
-  has_many   :event_recipients
+  has_many   :event_recipients, :dependent => :destroy
   has_many   :recipients, :class_name => "User", :through => :event_recipients
 
   validates_presence_of :creator, :eventable, :event_type
